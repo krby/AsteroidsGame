371cf00bba3efcf70a2ab7d2a4a71be94fda3095
kichoy <kichoy@697-334-Sauron.sf.k12.internal>
2014-11-05 11:35:59 -0800

    asteroids basic

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -15,13 +15,23 @@ import java.io.IOException;
 public class AsteroidsGame extends PApplet {
 
 SpaceShip ship1;
+
 Star[] stars;
+Asteroid[] asteroids;
 
 public void setup() 
 {
 	size (400, 400);
 	ship1 = new SpaceShip();
 
+	//asteroids
+	asteroids = new Asteroid[1];
+	for (int i = 0; i < asteroids.length; i++)
+	{
+		asteroids[i] = new Asteroid();
+	}
+
+	//stars
 	stars = new Star[100];
 	for (int i = 0; i < stars.length; i++)
 	{
@@ -32,10 +42,17 @@ public void setup()
 public void draw() 
 {
 	background(10, 40);
-	ship1.control(); //controlling keys
-	// ship1.hyperspace(); //hyperspace puts ship in new location
+	//ship
+	ship1.control(); //controlling keys wasd
 	ship1.move();
 	ship1.show();
+
+	//asteroids
+	for (int i = 0; i < asteroids.length; i++)
+	{
+		asteroids[i].move();
+		asteroids[i].show();
+	}
 
 	//stars
 	for (int i = 0; i < stars.length; i++)
@@ -46,28 +63,36 @@ public void draw()
 
 public void keyReleased()
 {
-	if (key == 32)
+	if (key == 32) //space
 	{
-		ship1.hyperspace();
+		ship1.hyperspace(); //hyerspace stops ship and puts in new location
 	}
+}
+public void mousePressed()
+{
+	setup();
+	redraw();
 }	
 
-//classes
+//classes----
 class SpaceShip extends Floater  
 {   
 	public SpaceShip()
 	{
 		//how the ship looks
-		corners = 3; 
+		corners = 4; 
 		xCorners = new int[corners];
 		yCorners = new int[corners];
-		//coordinates for the corners of ship 
-		xCorners[0] = -6;
-		yCorners[0] = -6;
-		xCorners[1] = 12;
-		yCorners[1] = 0;
-		xCorners[2] = -6;
-		yCorners[2] = 6;
+		//coordinates for the corners of ship
+		xCorners[0] = 0;
+		yCorners[0] = 0;		
+		xCorners[1] = -6;
+		yCorners[1] = -6;
+		xCorners[2] = 12;
+		yCorners[2] = 0;
+		xCorners[3] = -6;
+		yCorners[3] = 6;
+
 
 		//position related
 		myColor = color(150);
@@ -92,8 +117,8 @@ class SpaceShip extends Floater
 	public void control()
 	{
 		//controlling keys: w/s to forward/backward, d/a left/right
-		if (keyPressed && key == 'w') {accelerate(0.2f);} 
-		if (keyPressed && key == 's') {accelerate(-0.2f);}
+		if (keyPressed && key == 'w') {accelerate(0.15f);} 
+		if (keyPressed && key == 's') {accelerate(-0.15f);}
 		if (keyPressed && key == 'd') {rotate(3);}
 		if (keyPressed && key == 'a') {rotate(-3);}
 	}
@@ -106,7 +131,71 @@ class SpaceShip extends Floater
 		setDirectionX(0);
 		setDirectionY(0);
 		accelerate(0);
-		rotate((int)(Math.random()*360));	
+		rotate((int)(Math.random()*360));
+	}
+};
+
+class Asteroid extends Floater 
+{
+	private int rotSpd;
+	public Asteroid()
+	{
+		rotSpd = (int)(Math.random()*5)-2;
+		//asteroid appearance
+		corners = 3; 
+		xCorners = new int[corners];
+		yCorners = new int[corners];
+		//coordinates for the corners of ship 
+		xCorners[0] = -6;
+		yCorners[0] = -6;
+		xCorners[1] = 12;
+		yCorners[1] = 0;
+		xCorners[2] = -6;
+		yCorners[2] = 6;
+
+		//position related
+		myColor = color(150);
+		myCenterX = width/2;
+		myCenterY = height/2;
+		myDirectionX = (int)(Math.random()*5)-2;
+		myDirectionY = (int)(Math.random()*5)-2;
+		myPointDirection = (int)(Math.random()*5)-2;
+	}
+	public void setX(int x) {myCenterX = x;}
+	public int getX() {return (int)myCenterX;}
+	public void setY(int y) {myCenterY = y;}
+	public int getY() {return (int)myCenterY;}
+	public void setDirectionX(double x) {myDirectionX = x;}
+	public double getDirectionX() {return myDirectionX;}
+	public void setDirectionY(double y) {myDirectionY = y;}
+	public double getDirectionY() {return myDirectionY;}
+	public void setPointDirection(int degrees) {myPointDirection = degrees;}
+	public double getPointDirection() {return myPointDirection;}
+
+	public void move() //move the floater in the current direction of travel
+	{
+		rotate(rotSpd); //asteroid constantly rotates
+		//change the x and y coordinates by myDirectionX and myDirectionY       
+		myCenterX += myDirectionX;
+		myCenterY += myDirectionY;
+
+		//wrap around screen    
+		if(myCenterX >width)
+		{
+			myCenterX = 0;
+		}
+		else if (myCenterX<0)
+		{
+			myCenterX = width;
+		}
+		if(myCenterY >height)
+		{
+			myCenterY = 0;
+		}
+		else if (myCenterY < 0)
+		{
+			myCenterY = height;
+		}
 	}
 };
 
@@ -115,7 +204,7 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
 	protected int corners;  //the number of corners, a triangular floater has 3
 	protected int[] xCorners;
 	protected int[] yCorners;
-	protected int myColor;   
+	protected int myColor;
 	protected double myCenterX, myCenterY; //holds center coordinates   
 	protected double myDirectionX, myDirectionY; //holds x and y coordinates of the vector for direction of travel   
 	protected double myPointDirection; //holds current direction the ship is pointing in degrees
@@ -131,19 +220,19 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
 	abstract public double getPointDirection();
 
 	//Accelerates the floater in the direction it is pointing (myPointDirection)   
-	public void accelerate(double dAmount)   
+	public void accelerate(double dAmount)
 	{          
-		//convert the current direction the floater is pointing to radians    
-		double dRadians = myPointDirection*(Math.PI/180);     
-		//change coordinates of direction of travel    
-		myDirectionX += ((dAmount) * Math.cos(dRadians));    
-		myDirectionY += ((dAmount) * Math.sin(dRadians));       
+		//convert the current direction the floater is pointing to radians
+		double dRadians = myPointDirection*(Math.PI/180);
+		//change coordinates of direction of travel
+		myDirectionX += ((dAmount) * Math.cos(dRadians));
+		myDirectionY += ((dAmount) * Math.sin(dRadians));
 	}
 
 	public void rotate(int nDegreesOfRotation)   
 	{
 	//rotates the floater by a given number of degrees    
-	myPointDirection+=nDegreesOfRotation;   
+	myPointDirection+=nDegreesOfRotation;
 	}
 
 	public void move() //move the floater in the current direction of travel
@@ -154,39 +243,39 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
 
 		//wrap around screen    
 		if(myCenterX >width)
-		{     
+		{
 			myCenterX = 0;
-		}    
+		}
 		else if (myCenterX<0)
 		{
-			myCenterX = width;    
+			myCenterX = width;
 		}
 		if(myCenterY >height)
 		{
-			myCenterY = 0;    
+			myCenterY = 0;
 		}
 		else if (myCenterY < 0)
 		{
-			myCenterY = height;    
+			myCenterY = height;
 		}
 	}
 
-	public void show()  //Draws the floater at the current position  
-	{             
-		fill(myColor);   
-		stroke(myColor);    
-		//convert degrees to radians for sin and cos         
+	public void show()  //Draws the floater at the current position
+	{
+		fill(myColor);
+		stroke(myColor);
+		//convert degrees to radians for sin and cos
 		double dRadians = myPointDirection*(Math.PI/180);
-		int xRotatedTranslated, yRotatedTranslated;    
-		beginShape();         
+		int xRotatedTranslated, yRotatedTranslated;
+		beginShape();
 		for(int nI = 0; nI < corners; nI++)
 		{
 			//rotate and translate the coordinates of the floater using current direction 
-			xRotatedTranslated = (int)((xCorners[nI]* Math.cos(dRadians)) - (yCorners[nI] * Math.sin(dRadians))+myCenterX);     
-			yRotatedTranslated = (int)((xCorners[nI]* Math.sin(dRadians)) + (yCorners[nI] * Math.cos(dRadians))+myCenterY);      
-			vertex(xRotatedTranslated,yRotatedTranslated);    
+			xRotatedTranslated = (int)((xCorners[nI]* Math.cos(dRadians)) - (yCorners[nI] * Math.sin(dRadians))+myCenterX);
+			yRotatedTranslated = (int)((xCorners[nI]* Math.sin(dRadians)) + (yCorners[nI] * Math.cos(dRadians))+myCenterY);
+			vertex(xRotatedTranslated,yRotatedTranslated);
 		}
-		endShape(CLOSE);  
+		endShape(CLOSE);
 	}
 } 
 
